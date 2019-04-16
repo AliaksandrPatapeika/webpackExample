d07acae25417b5e37466b6476581b90d5f7f5f03
Aliaksandr Patapeika <Aliaksandr_Patapeika@epam.com>
2019-04-16 21:55:15 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '\\dist'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      }
+    ]
+  }
+};
