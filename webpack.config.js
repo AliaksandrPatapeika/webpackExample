174f3d567a365fe7f3de13b17248adf7872e19ad
Aliaksandr Patapeika <Aliaksandr_Patapeika@epam.com>
2019-04-17 09:38:22 +0300

    config modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
 
+// console.log('path = ', path.resolve(__dirname, 'dist'));
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
